6edd66c16bbfd19b3c56f75713649f15139c11d7
kyphosis <mikk@indieleaf.io>
2018-04-16 21:01:31 +0300

    make it better

--- app.js
+++ app.js
@@ -5,14 +5,47 @@ const express = require('express')
 const app = express()
 var favicon = require('serve-favicon')
 var path = require('path')
+const nodemon = require ('nodemon')
 
 
 // Static folder
 app.use(express.static('public'))
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico'))) 
+app.use(favicon(path.join(__dirname, '/public', 'favicon.ico'))) 
 
 
+
+app.get('/services',function(req,res){
+  res.sendFile(path.join(__dirname+'/public/services.html'));
+});
+
+app.get('/cookies',function(req, res) {
+  res.sendFile(path.join(__dirname+'/public/cookies.html'));
+})
+
+app.get('/design',function(req, res) {
+  res.sendFile(path.join(__dirname+'/public/design.html'));
+})
+
+app.get('/contact',function(req, res) {
+  res.sendFile(path.join(__dirname+'/public/contact.html'));
+})
+
+app.get('/team',function(req, res) {
+  res.sendFile(path.join(__dirname+'/public/team.html'));
+})
+
+app.get('/portfolio',function(req, res) {
+  res.sendFile(path.join(__dirname+'/public/portfolio.html'));
+})
+
+app.get('/inspire',function(req, res) {
+  res.sendFile(path.join(__dirname+'/public/inspire.html'));
+})
+
+app.get('/company',function(req, res) {
+  res.sendFile(path.join(__dirname+'/public/company.html'));
+})
  
-   app.listen(process.env.PORT || 3000, function() {
-  console.log("listening on 3000");
+app.listen(3000, '0.0.0.0', function() {
+  console.log('Listening to port:  ' + 3000);
 });
