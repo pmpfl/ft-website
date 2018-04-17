75175aac384c62f1be4470652c8a48e87f6b94ec
kyphosis <mikk@indieleaf.io>
2018-04-17 23:37:14 +0300

    first commit

--- app.js
+++ app.js
@@ -1,51 +1,108 @@
-/**
- * @Author Mikk Küttim
- */
 const express = require('express')
 const app = express()
 var favicon = require('serve-favicon')
 var path = require('path')
 const nodemon = require ('nodemon')
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+
 
 
 // Static folder
 app.use(express.static('public'))
 app.use(favicon(path.join(__dirname, '/public', 'favicon.ico'))) 
 
+app.use(bodyParser.urlencoded({ extended: false })); 
+app.use(bodyParser.json())
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.render('contact');
+  });
+  
+  app.post('/contact', (req, res) => {
+    const output = `
+      <p>You have a new message from contact form.</p>
+      <h3>Contact Details</h3>
+      <ul>  
+        <li>Name: ${req.body.name}</li>
+        <li>Email: ${req.body.email}</li>
+      </ul>
+      <h3>Message</h3>
+      <p>${req.body.message}</p>
+    `;
+  
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: 'smtp.ethereal.email',
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+          user: 'i3brpyll74gl4m6v@ethereal.email', // generated ethereal user
+          pass: 'bMC1hmYKaxKtRr53mj'  // generated ethereal password
+      },
+      tls:{
+        rejectUnauthorized:false
+      }
+    });
+  
+    // setup email data with unicode symbols
+    let mailOptions = {
+        from: '"form@fitric.ee', // sender address
+        to: 'mikk@fitric.ee', // list of receivers
+        subject: 'Contact Request', // Subject line
+        text: 'Hello world?', // plain text body
+        html: output // html body
+    };
+  
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);   
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+  
+        res.render('contact', {msg:'Email has been sent'});
+    });
+    });
 
 
-app.get('/services',function(req,res){
-  res.sendFile(path.join(__dirname+'/public/services.html'));
+app.get('/contact',function(req,res){
+  res.sendFile(path.join(__dirname+'/public/contact.html'));
 });
 
-app.get('/cookies',function(req, res) {
-  res.sendFile(path.join(__dirname+'/public/cookies.html'));
-})
 
-app.get('/design',function(req, res) {
-  res.sendFile(path.join(__dirname+'/public/design.html'));
-})
 
-app.get('/contact',function(req, res) {
-  res.sendFile(path.join(__dirname+'/public/contact.html'));
-})
+/**
+ * Portfolio subdirectories
+ */
+app.get('/portfolio/ieep',function(req,res){
+  res.sendFile(path.join(__dirname+'/public/portfolio/ieep.html'));
+});
 
-app.get('/team',function(req, res) {
+app.get('/portfolio/givedish',function(req,res){
+  res.sendFile(path.join(__dirname+'/public/portfolio/givedish.html'));
+});
+
+app.get('/team',function(req,res){
   res.sendFile(path.join(__dirname+'/public/team.html'));
-})
+});
 
-app.get('/portfolio',function(req, res) {
-  res.sendFile(path.join(__dirname+'/public/portfolio.html'));
-})
+app.get('/jobs/join-us/',function(req,res){
+  res.sendFile(path.join(__dirname+'/public/jobs/join-us.html'));
+});
 
-app.get('/inspire',function(req, res) {
+app.get('/inspire/',function(req,res){
   res.sendFile(path.join(__dirname+'/public/inspire.html'));
-})
+});
 
-app.get('/company',function(req, res) {
-  res.sendFile(path.join(__dirname+'/public/company.html'));
-})
- 
+app.get('/cookies/',function(req,res){
+  res.sendFile(path.join(__dirname+'/public/cookies.html'));
+});
 app.listen(process.env.PORT || 3000, function() {
   console.log("listening on 3000");
 });
